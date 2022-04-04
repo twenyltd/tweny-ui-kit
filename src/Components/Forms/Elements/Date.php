7aff896b6ef7020f472b3a61c37fe9acbe93f492
antweny <antweny@live.com>
2022-04-04 12:53:25 +0300

    add form input date component

---
+++ src/Components/Forms/Elements/Date.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TwenyCode\TwenyUiKit\Components\Forms\Elements;
+
+class Date extends  Input
+{
+    public function __construct($name = 'date', $id = null)
+    {
+        parent::__construct($name, $id, 'date');
+    }
+
+
+    //  Get the view / contents that represent the component.
+    public function render()
+    {
+        return view('tweny-ui-kit-views::components.forms.elements.date');
+    }
+}
