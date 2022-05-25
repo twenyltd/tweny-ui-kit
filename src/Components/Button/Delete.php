c498d0565f62ecb9f64c2617d72b875658bd6fcd
antweny <antweny@live.com>
2022-05-25 12:28:00 +0300

    Update Delete.php

--- src/Components/Button/Delete.php
+++ src/Components/Button/Delete.php
@@ -13,11 +13,14 @@ class Delete extends Component
     /* @var  */
     public $label;
 
+    public $class;
+
     //  Create a new component instance.
     public function __construct($action, $label = '<i class="fa fa-trash-alt"></i> Delete', $class="btn-block")
     {
         $this->action = $action;
         $this->label = $label;
+        $this->class = $class;
     }
 
     //  Get the view / contents that represent the component.
