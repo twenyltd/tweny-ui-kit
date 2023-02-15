2c71fb94360307132964dc78acfafc3c4b1cadc3
antweny <antweny@live.com>
2023-02-15 16:51:39 +0300

    Update dropleft.blade.php
    make dynamic dropdown

--- resources/views/components/button/dropleft.blade.php
+++ resources/views/components/button/dropleft.blade.php
@@ -1,4 +1,4 @@
-<div class="btn-group dropleft">
+<div class="btn-group {{$align}}">
 
     <button {{$attributes->merge(['class'=>'btn'])}} style=".btn:hover{border:0;}" data-toggle="dropdown">
         <i class="{{$icon}}"></i>
