65782daeada7c0a8b77e564048a60d19a9be07b9
antweny <antweny@live.com>
2023-02-17 01:18:58 +0300

    Update Dropleft.php

--- src/Components/Button/Dropleft.php
+++ src/Components/Button/Dropleft.php
@@ -8,11 +8,14 @@ use Illuminate\View\Component;
 class Dropleft extends Component
 {
     public $icon;
-    
+
+    public $align;
+
     //  Create a new component instance.
-    public function __construct( $class="btn btn-link btn-sm text-dark", $icon = 'fa-solid fa-ellipsis-vertical' )
+    public function __construct( $class="btn btn-link btn-sm text-dark", $icon = 'fa-solid fa-ellipsis-vertical', $align = 'dropstart' )
     {
         $this->icon = $icon;
+        $this->align = $align;
     }
 
     //  Get the view / contents that represent the component.
