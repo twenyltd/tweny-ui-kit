d6e69a96f4c765f8e280832c67568ffe981b30b1
TWENY Limited <129292622+twenyltd@users.noreply.github.com>
2024-08-20 01:41:23 +0300

    Update table.blade.php

--- resources/views/components/tables/table.blade.php
+++ resources/views/components/tables/table.blade.php
@@ -1,6 +1,6 @@
 <div class="table-responsive">
 
-    <table {{$attributes->merge(['class'=>'table table-responsive'])}} id="{{$id}}" >
+    <table {{$attributes->merge(['class'=>'table'])}} id="{{$id}}" >
 
         <thead class="text-center">
             <tr>
